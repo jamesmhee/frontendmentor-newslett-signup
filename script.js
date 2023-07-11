bccb62898455827ebb78b3d75126b039496d082f
jamesmhee <nontasabs@gmail.com>
2023-07-12 02:21:58 +0700

    100%

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-const valid_email = document.getElementById('valid');
-const input_email = document.getElementById('email-input');
+const valid_Email = document.getElementById('valid');
+const input_Email = document.getElementById('email-input');
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const success_Email = document.getElementById('success')
+const subscribe_Email = document.getElementById('subscribe')
+const email_Sub = document.getElementById('success-email')
 
 function validate_email(){
-        if(!re.test(input_email.value)){
-            console.log('invalid email');
-            valid_email.classList.add('active');
+        if(!re.test(input_Email.value)){
+            console.log('invalid Email');
+            valid_Email.classList.add('active');
             return false;
         } else {
             console.log('valid email');
-            valid_email.classList.remove('active');
+            valid_Email.classList.remove('active');
+            email_Sub.innerHTML = `<strong>${input_Email.value}</strong>`;
+            console.log(email_Sub.innerHTML)
             return true;
         }
+}
+
+function successEmail(event){    
+    event.preventDefault()
+    if(validate_email()){
+        subscribe_Email.classList += ' display-none'
+        success_Email.classList -= ' display-none'        
+    } else {
+        success_Email.classList += ' display-none'
     }
+}
+
